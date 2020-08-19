5dc4d1149f912c203e2f05555cbef955b386a2d7
soyoung00 <parksy9750@naver.com>
2020-08-19 15:32:50 +0900

    inter_step2

--- src/com/tech2/poly/HRSTest.java
+++ src/com/tech2/poly/HRSTest.java
@@ -8,6 +8,7 @@ abstract class Employee {
 	public abstract void calcBonus();
 }
 	class Salesman extends Employee{
+		int annual_sales;
 		public void calcSalary() {
 			System.out.println("Salesman 급여= 기본급 + 판매 수당");
 		}
@@ -18,6 +19,7 @@ abstract class Employee {
 		}
 
 	class Consultant extends Employee{
+		int num_project;
 		public void calcSalary() {
 			System.out.println("Consultant 급여 = 기본급 + 컨설팅 특별 수당");
 		}
@@ -26,6 +28,7 @@ abstract class Employee {
 		}
 	}
 	abstract class Manager extends Employee{
+		int num_team;
 		public void calcSalary() {
 			System.out.println("Manager 급여 = 기본급 + 팀 성과 수당");
 			
@@ -38,15 +41,24 @@ abstract class Employee {
 
 	}
 	public class HRSTest{
-		public static void calTax(Employee s) {//다형성/332p/Employee가 부모니까 다양한 형식을 받아들임
+		public static void calTax(Employee e) {//다형성/332p/Employee가 부모니까 다양한 형식을 받아들임
 //			System.out.println("소득세를 계산합니다.");
-			System.out.println("Saleman비교:"+(s instanceof Salesman));
-			if (s instanceof Salesman) {
-				System.out.println("Saleman입니다");
-			}else if (s instanceof Manager) {
-				System.out.println("Saleman입니다");
-			}else if (s instanceof Consultant) {
-				System.out.println("Saleman입니다");
+			System.out.println("Saleman비교:"+(e instanceof Salesman));
+			if (e instanceof Salesman) {
+				Salesman s=(Salesman)e;//자식/형변환/e=employee
+				s.annual_sales=650000;
+				System.out.println("Saleman 입니다"+s.annual_sales);
+//				System.out.println("Saleman입니다");
+			}else if (e instanceof Manager) {
+//				System.out.println("Saleman입니다");
+				Manager m=(Manager)e;//자식/형변환
+				m.num_team=5;
+				System.out.println("Manager 입니다"+m.num_team);
+			}else if (e instanceof Consultant) {
+//				System.out.println("Saleman입니다");
+				Consultant c=(Consultant)e;//자식/형변환
+				c.num_project=35;
+				System.out.println("Consultant입니다"+c.num_project);
 				}
 		}
 		
